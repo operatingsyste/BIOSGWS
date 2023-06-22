52efd084ea4d78673db01cf39eaf8d1277193b2d
operatingsyste <134151646+operatingsyste@users.noreply.github.com>
2023-06-22 17:36:47 +0530

    Update main.js

--- main.js
+++ main.js
@@ -6,15 +6,19 @@ function openServer(){
 }
 
 function GWS2(){
-  window.open('https://gws2.operatingsyste.repl.co/index1.html');
+  window.open('https://operatingsyste.github.io/GWS2/');
+}
+
+function GWS3(){
+  window.open('https://operatingsyste.github.io/GWS3/');
 }
 
 function GWS(){
-  window.open('https://gws.operatingsyste.repl.co/index1.html');
+  window.open('https://operatingsyste.github.io/GWS/');
 }
 
 function GWSHOLISUPER() {
-  window.open('https://gwsholisuper.operatingsyste.repl.co');
+  window.open('https://operatingsyste.github.io/gws-holi-super/');
 }
 
 function save(){
@@ -23,23 +27,99 @@ function save(){
     //alert('BOOTING... GWS ');
     GWS()
   }
-  else if(getOs === 'GWS 2'){
-   // alert('GWS 2');
-    GWS2();
+  else if (getOs === 'GWS2'){
+    //alert('BOOTING... GWS ');
+    GWS2()
   }
-  else if (getOs === 'GWS HOLI SUPER'){
-    //alert('GHS');
-    GWSHOLISUPER();
+
+  else if (getOs === 'GWS3'){
+    //alert('BOOTING... GWS ');
+    GWS3()
   }
+
+   else if (getOs === 'GWS HOLI SUPER'){
+    //alert('BOOTING... GWS ');
+    GWSHOLISUPER()
+  }
+  else if (getOs === 'GWS4'){
+GWS4();
+  }
+
+  else if (getOs === 'GWS5'){
+GWS5();
+  }
+ else if (getOs === 'GWS6'){
+GWS6();
+  }
+   else if (getOs === 'GWS7'){
+GWS7();
+  }
+else if (getOs === 'GWS8'){
+GWS8();
 }
 
-function gws(){
-  var getOs = document.getElementById('os').value = 'GWS';
-}
 
-function gws2(){
-  var getOs = document.getElementById('os').value = 'GWS 2';
 }
 function gwsholisuper() {
   var getOs = document.getElementById('os').value = 'GWS HOLI SUPER';
+}
+
+function gws3() {
+  var getOs = document.getElementById('os').value = 'GWS3';
+}
+
+function gws2() {
+  var getOs = document.getElementById('os').value = 'GWS2';
+}
+
+function gws() {
+  var getOs = document.getElementById('os').value = 'GWS';
+}
+
+function gws4() {
+  var getOs = document.getElementById('os').value = 'GWS4';
+}
+
+function gws5() {
+  var getOs = document.getElementById('os').value = 'GWS5';
+}
+
+    function GWS4(){
+  window.open('https://operatingsyste.github.io/GWS4/');
+    }
+
+    function GWS5(){
+  window.open('https://operatingsyste.github.io/GWS5/');
+    }
+
+
+
+function gws7() {
+  var getOs = document.getElementById('os').value = 'GWS7';
+}
+
+function GWS7(){
+  window.open('https://operatingsyste.github.io/GWS7/');
+    }
+
+
+
+
+
+
+function gws6() {
+  var getOs = document.getElementById('os').value = 'GWS6';
+}
+
+function GWS6(){
+  window.open('https://operatingsyste.github.io/GWS6/');
+    }
+
+
+                                      function gws8() {
+  var getOs = document.getElementById('os').value = 'GWS8';
+}
+
+function GWS8(){
+  window.open('https://operatingsyste.github.io/GWS8/');
 }
